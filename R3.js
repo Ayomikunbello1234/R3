059cdae7987ec16e864cc3ce46f78a8e9b01ada3
Ayomikun <ayo04bellol@gmail.com>
2026-08-22 01:22:45 +0100

    new changes

--- R3.js
+++ R3.js
@@ -13,3 +13,45 @@ setTimeout(() => {
     changing.style.opacity = 1;
 }, 500);
 },2000);
+
+const carouselCard = document.querySelector(".carousel-card");
+const slides = document.querySelectorAll(".mission-img");
+const dots = document.querySelectorAll(".carousel-dots .dots");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+
+    carouselCard.style.transform = `translateX(-${index * 500}px)`;
+
+    dots.forEach(dot => {
+        dot.classList.remove("active");
+    });
+
+    dots[index].classList.add("active");
+}
+
+function nextSlide() {
+
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 4000);
+
+const sections = document.querySelectorAll(".reveal");
+
+window.addEventListener( "scroll",  () => {
+    sections.forEach(section => {
+        const position = section.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            section.classList.add("show");
+        }
+    });
+})
